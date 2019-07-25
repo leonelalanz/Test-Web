da80f2a39b6047b0f79123acba0cb83e1c4075ca
cuero15 <christian.cuero@gmail.com>
2019-07-24 21:07:26 -0400

    Admision Christian Cuero

--- src/module/component/login.component.js
+++ src/module/component/login.component.js
@@ -4,15 +4,53 @@ angular.module('app')
 	.component('login', {
 		templateUrl: '/src/templates/login.html',
 		controller: function ($http, $scope) {
-			let $this = $scope;
-			let username = $this.username;
-			let password = $this.password;
+			//let $this = $scope;
+
+
 			let types = [
 				{ 'name': 'V' },
 				{ 'name': 'E' },
 				{ 'name': 'P' },
 			]
-			$this.items = types;
+			$scope.items = types;
+
+			$scope.send = function () {
+
+				let form = [];
+				form.push({
+					"username": $scope.username,
+					"password": $scope.password,
+					"type": $scope.selected
+				});
+				console.log(form);
+
+
+				//$http POST function
+				$http({
+
+					method: 'POST',
+					url: 'https://prueba-admision-web.herokuapp.com/session',
+					data: form
+
+				}).then(function successCallback(response) {
+
+					console.log(response);
+					alert("User has Logging Successfully")
+
+				}, function errorCallback(response) {
+					console.log(response.data);
+
+
+					$scope.username = '';
+					$scope.password = '';
+					alert("Error. while Logging user Try Again!");
+
+				});
+
+
+			}
+
+
 
 		}
 	})
